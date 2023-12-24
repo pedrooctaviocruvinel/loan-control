2c25b06a1cddacf1a8c5620bd8cb3824d9642b46
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2023-12-23 22:34:17 -0300

    eslint and prettier support added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,4 +8,4 @@ import { RouterOutlet } from '@angular/router';
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent { }
+export class AppComponent {}

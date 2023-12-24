3fe03eb346151cf3c4ddc2c750dcde2725fdc562
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2023-12-23 22:47:11 -0300

    angular material added

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { LoanComponent } from './loan/loan.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'loans', component: LoanComponent }];
---
+++ client/src/app/loan/loan.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-loan',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './loan.component.html',
+})
+export class LoanComponent {}

ba67f3dcef098d8021c628f26cc751e481b2817c
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2023-12-29 11:28:05 -0300

    add payment dialog refactored

--- client/src/app/loans/components/add-payment-dialog/add-payment-dialog.component.ts
+++ client/src/app/loans/components/add-payment-dialog/add-payment-dialog.component.ts
@@ -1,4 +1,3 @@
-import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, Inject } from '@angular/core';
 import {
 	FormBuilder,
@@ -10,17 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import {
 	MAT_DIALOG_DATA,
-	MatDialogActions,
-	MatDialogClose,
-	MatDialogContent,
 	MatDialogModule,
 	MatDialogRef,
-	MatDialogTitle,
 } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatStepperModule } from '@angular/material/stepper';
 
 import { environment } from '@/environments/environment';
 import { headers } from '@/shared/headers';
@@ -30,27 +24,16 @@ import { ResultWrapper } from '@/shared/types/resultWrapper';
 import { AddPaymentRequest } from './types/addPaymentRequest';
 
 @Component({
-	selector: 'app-create-loan-dialog',
+	selector: 'app-add-payment-dialog',
 	standalone: true,
 	imports: [
 		MatInputModule,
 		MatButtonModule,
-		MatDialogTitle,
-		MatDialogContent,
-		MatDialogActions,
-		MatDialogClose,
 		ReactiveFormsModule,
-		MatStepperModule,
 		MatSlideToggleModule,
 		MatDatepickerModule,
 		MatDialogModule,
-		MatDividerModule,
-	],
-	providers: [
-		{
-			provide: STEPPER_GLOBAL_OPTIONS,
-			useValue: { showError: true },
-		},
+		MatIconModule,
 	],
 	templateUrl: './add-payment-dialog.component.html',
 })
@@ -59,24 +42,22 @@ export class AddPaymentDialogComponent {
 		@Inject(MAT_DIALOG_DATA) private loanId: string,
 		public dialogRef: MatDialogRef<AddPaymentDialogComponent>,
 		public formBuilder: FormBuilder
-	) {
-		this.addPaymentForm = formBuilder.group({
-			value: ['', [Validators.required, Validators.min(1)]],
-			paid: [''],
-			expirationDate: ['', [Validators.required]],
-		});
-	}
+	) {}
 
-	addPaymentForm: FormGroup;
+	addPaymentForm: FormGroup = this.formBuilder.group({
+		value: ['', [Validators.required, Validators.min(1)]],
+		paid: [''],
+		expirationDate: ['', [Validators.required]],
+	});
 
-	async addPaymentToLoan(): Promise<void> {
+	async addPayment(): Promise<void> {
 		const addPaymentRequest: AddPaymentRequest = {
 			value: this.addPaymentForm.value.value,
 			paid: this.addPaymentForm.value.paid == true ? true : false,
 			expirationDate: this.addPaymentForm.value.expirationDate,
 		};
 
-		const addLoanResponse = await fetch(
+		const addPaymentResponse = await fetch(
 			`${environment.serverUrl}/loans/${this.loanId}/payments`,
 			{
 				method: 'POST',
@@ -85,11 +66,12 @@ export class AddPaymentDialogComponent {
 			}
 		);
 
-		const addLoanResult: ResultWrapper<void> = await addLoanResponse.json();
+		const addPaymentResult: ResultWrapper<void> =
+			await addPaymentResponse.json();
 
 		const dialogResult: DialogResult = {
-			success: addLoanResult.success,
-			errors: addLoanResult.errors,
+			success: addPaymentResult.success,
+			errors: addPaymentResult.errors,
 		};
 
 		this.dialogRef.close(dialogResult);

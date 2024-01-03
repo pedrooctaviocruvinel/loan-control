8fe5aa3c5cba19a087cb65f2f15f66ac7020129d
pedrooctaviocruvinel <pedrooctaviocruvinel@gmail.com>
2024-01-02 21:18:02 -0300

    locales added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { Component, DEFAULT_CURRENCY_CODE, LOCALE_ID } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+
+registerLocaleData(ptBr);
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
 	imports: [CommonModule, RouterOutlet, MatButtonModule],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'pt' },
+		{ provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+	],
 	templateUrl: './app.component.html',
 })
 export class AppComponent {
